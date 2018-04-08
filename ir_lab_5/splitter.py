b58d37f3dbf171cef70e581c0ec8fc50caa5163d
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-04-08 14:50:47 +0530

    splitter complete

--- ir_lab_5/splitter.py
+++ ir_lab_5/splitter.py
@@ -21,4 +21,22 @@ fh.close()
 for key in model_out.keys():
 	model_out[key].sort(key=operator.itemgetter(2),reverse=True)
 
+# reading qrel file
+qrel = dict()
+
+fh = open('./qrels.test')
+for line in fh:
+	result = line.split()
+	qId = result[0]
+	doc = result[2]
+	relInfo = result[3]
+
+	if qId not in qrel.keys():
+		qrel[qId] = dict()
+	else:
+		qrel[qId][doc] = relInfo
+
+
 print( model_out['301'] )
+print()
+print( qrel['301'] )
